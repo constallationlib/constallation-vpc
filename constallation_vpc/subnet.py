ca38be4c5c31f0b3e08f739836d1389737e449bc
CoulterStutz <coultercash@gmail.com>
2024-08-07 23:29:33 -0600

    FIX MOBILE EDITING ERRORS

--- constallation_vpc/subnet.py
+++ constallation_vpc/subnet.py
@@ -21,3 +21,11 @@ class Subnet(_vpc):
 
         super().__init__(region=region, aws_access_key=self._aws_access_key, aws_access_secret_key=self._aws_access_secret_key, aws_sts_session_token=self._aws_sts_token)
         del self._aws_sts_token, self._aws_access_key, self._aws_access_secret_key
+        self._initualize_subnet()
+
+    def _initualize_subnet(self):
+        print(super().describe_subnet(s))
+
+
+if __name__ == '__main__':
+    x = Subnet(region='us-west-1', subnet_id='subnet-0c0036e53c417c552')
