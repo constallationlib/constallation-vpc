d2a76010404199453fb6267ba5b74d57a4ef4ee0
CoulterStutz <coultercash@gmail.com>
2024-08-09 17:16:58 -0600

    Auto-commit at Fri Aug  9 05:16:58 PM MDT 2024

--- constellation_vpc/_vpc.py
+++ constellation_vpc/_vpc.py
@@ -2,20 +2,15 @@ import subprocess as _subprocess
 import json
 
 class _vpc:
-    def __init__(self, region: str = None, aws_access_key: str = None, aws_access_secret_key: str = None,
-                 aws_sts_session_token: str = None):
+    def __init__(self, region: str = None, aws_access_key: str = None, aws_access_secret_key: str = None):
         self.region_name = region
         self._access_key = aws_access_key
         self._secret_key = aws_access_secret_key
-        self._session_token = aws_sts_session_token
 
     def _run_aws_command(self, cmd: list) -> dict:
         if self._access_key and self._secret_key:
             cmd.extend(["--aws-access-key-id", self._access_key])
             cmd.extend(["--aws-secret-access-key", self._secret_key])
-
-        if self._session_token:
-            cmd.extend(["--aws-session-token", self._session_token])
 
         process = _subprocess.Popen(cmd, stdout=_subprocess.PIPE, stderr=_subprocess.PIPE, text=True)
         stdout, stderr = process.communicate()
