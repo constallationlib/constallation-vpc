4d7fe205dec194319154c2fc59123af97592fd20
CoulterStutz <coultercash@gmail.com>
2024-08-09 17:05:30 -0600

    VERS CHANGE

--- constellation_vpc/__init__.py
+++ constellation_vpc/__init__.py
@@ -3,6 +3,6 @@ __version__ = "0.1.2a0"
 __author__ = "Coulter Stutz"
 __email__ = "coulterstutz@gmail.com"
 __license__ = "MIT"
-from subnet import Subnet
-from vpc import VPC
+from .subnet import Subnet
+from .vpc import VPC
 __all__ = ["Subnet", "VPC"]
