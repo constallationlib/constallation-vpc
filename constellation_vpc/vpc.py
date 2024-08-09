e61dfed18a1da026690064825fa91a1b609678e3
CoulterStutz <coultercash@gmail.com>
2024-08-09 14:09:17 -0600

    Auto-commit at Fri Aug  9 02:09:17 PM MDT 2024

--- constellation_vpc/vpc.py
+++ constellation_vpc/vpc.py
@@ -41,4 +41,69 @@ class VPC(_vpc):
         return vpc
 
     def delete_vpc(self, vpc_id: str = None) -> dict:
-        vpc_id = vpc_id
+        vpc_id = vpc_id or self._vpc_id
+        result = super()._delete_vpc(vpc_id)
+        if "Error" in result:
+            self._error_handler.parse_and_raise(result)
+        return result
+
+    def associate_cidr_block(self, cidr_block: str) -> dict:
+        result = super()._associate_vpc_cidr_block(self._vpc_id, cidr_block)
+        if "Error" in result:
+            self._error_handler.parse_and_raise(result)
+        return result
+
+    def disassociate_cidr_block(self, association_id: str) -> dict:
+        result = super()._disassociate_vpc_cidr_block(association_id)
+        if "Error" in result:
+            self._error_handler.parse_and_raise(result)
+        return result
+
+    def describe_cidr_reservations(self) -> dict:
+        result = super()._describe_vpc_cidr_reservations(self._vpc_id)
+        if "Error" in result:
+            self._error_handler.parse_and_raise(result)
+        return result
+
+    def modify_attribute(self, attribute_name: str, attribute_value) -> dict:
+        result = super()._modify_vpc_attribute(self._vpc_id, attribute_name, attribute_value)
+        if "Error" in result:
+            self._error_handler.parse_and_raise(result)
+        return result
+
+    def describe_vpc(self, vpc_id: str = None) -> dict:
+        vpc_id = vpc_id or self._vpc_id
+        result = super()._describe_vpc(vpc_id)
+        if "Error" in result:
+            self._error_handler.parse_and_raise(result)
+        return result
+
+    @property
+    def cidr_block(self):
+        return self._cidr_block
+
+    @property
+    def is_default(self):
+        return self._is_default
+
+    @property
+    def state(self):
+        return self._state
+
+    @property
+    def owner_id(self):
+        return self._owner_id
+
+    @property
+    def vpc_id(self):
+        return self._vpc_id
+
+    def __del__(self):
+        # Cleanup resources if needed
+        for attr in list(self.__dict__.keys()):
+            delattr(self, attr)
+
+
+if __name__ == '__main__':
+    vpc = VPC('us-west-2', vpc_id="vpc-017f9600d16474436")
+    print(vpc.describe_vpc())
