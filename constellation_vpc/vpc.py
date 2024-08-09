468cb694a768f27161c84e62da4f9a18132180be
CoulterStutz <coultercash@gmail.com>
2024-08-09 14:08:56 -0600

    Auto-commit at Fri Aug  9 02:08:56 PM MDT 2024

--- constellation_vpc/vpc.py
+++ constellation_vpc/vpc.py
@@ -1,4 +1,5 @@
 from _vpc import _vpc
+from errors import ErrorHandler
 
 class VPC(_vpc):
     def __init__(self, region: str, vpc_id: str = None, aws_access_key: str = None, aws_access_secret_key: str = None,
@@ -9,6 +10,8 @@ class VPC(_vpc):
         self._aws_access_secret_key = aws_access_secret_key
         self._aws_sts_token = aws_session_token
         self._vpc_cidr_block = vpc_cidr_block
+        self._error_handler = ErrorHandler()
+
         super().__init__(region=region, aws_access_key=self._aws_access_key,
                          aws_access_secret_key=self._aws_access_secret_key, aws_sts_session_token=self._aws_sts_token)
         del self._aws_sts_token, self._aws_access_key, self._aws_access_secret_key
@@ -18,7 +21,7 @@ class VPC(_vpc):
         if self._vpc_id is not None:
             vpc = super()._describe_vpc(self._vpc_id)
             if "Error" in vpc:
-                raise Exception(vpc["Error"])
+                self._error_handler.parse_and_raise(vpc)
             self._cidr_block = vpc.get('CidrBlock')
             self._is_default = vpc.get('IsDefault')
             self._state = vpc.get('State')
@@ -26,54 +29,16 @@ class VPC(_vpc):
         elif self._vpc_cidr_block is not None:
             vpc = super()._create_vpc(self._vpc_cidr_block)
             if "Error" in vpc:
-                raise Exception(vpc["Error"])
+                self._error_handler.parse_and_raise(vpc)
             self._vpc_id = vpc.get('VpcId')
-            self._cidr_block = self._vpc_cidr_block
             self._initialize_vpc()
-
-    def describe_vpc(self, vpc_id: str = None) -> dict:
-        if vpc_id is not None:
-            return super()._describe_vpc(vpc_id)
-        return super()._describe_vpc(self._vpc_id)
 
     def create_vpc(self, cidr_block: str) -> dict:
         vpc = super()._create_vpc(cidr_block)
         if "Error" in vpc:
-            raise Exception(vpc["Error"])
+            self._error_handler.parse_and_raise(vpc)
+        self._vpc_id = vpc.get('VpcId')
         return vpc
 
     def delete_vpc(self, vpc_id: str = None) -> dict:
-        if vpc_id:
-            return super()._delete_vpc(vpc_id)
-        return super()._delete_vpc(self._vpc_id)
-
-    def associate_vpc_cidr_block(self, cidr_block: str, vpc_id: str = None) -> dict:
-        if vpc_id is None:
-            vpc_id = self._vpc_id
-        return super()._associate_vpc_cidr_block(vpc_id, cidr_block)
-
-    def disassociate_vpc_cidr_block(self, association_id: str) -> dict:
-        return super()._disassociate_vpc_cidr_block(association_id)
-
-    def modify_vpc_attribute(self, attribute_name: str, attribute_value) -> dict:
-        return super()._modify_vpc_attribute(self._vpc_id, attribute_name, attribute_value)
-
-    def describe_vpc_cidr_reservations(self) -> dict:
-        return super()._describe_vpc_cidr_reservations(self._vpc_id)
-
-    def create_subnet(self, cidr_block: str, availability_zone: str) -> dict:
-        return super()._create_subnet(self._vpc_id, cidr_block, availability_zone)
-
-    def delete_subnet(self, subnet_id: str) -> dict:
-        return super()._delete_subnet(subnet_id)
-
-    def describe_subnet(self, subnet_id: str) -> dict:
-        return super()._describe_subnet(subnet_id)
-
-    def modify_subnet_attribute(self, subnet_id: str, attribute_name: str, attribute_value) -> dict:
-        return super()._modify_subnet_attribute(subnet_id, attribute_name, attribute_value)
-
-
-if __name__ == '__main__':
-    vpc = VPC('us-west-2', vpc_id="vpc-017f9600d16474436")
-    print(vpc.describe_vpc())
+        vpc_id = vpc_id
