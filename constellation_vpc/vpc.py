585643be3912354e75f0326c17ee6b2cd206629d
CoulterStutz <coultercash@gmail.com>
2024-08-09 13:52:25 -0600

    Auto-commit at Fri Aug  9 01:52:25 PM MDT 2024

--- constellation_vpc/vpc.py
+++ constellation_vpc/vpc.py
@@ -1,1 +1,9 @@
 from _vpc import _vpc
+
+class VPC(_vpc):
+    def __init__(self, region:str, vpc_id:str=None, aws_access_key:str=None, aws_access_secret_key:str=None, vpc_cidr_block:str=None):
+        self._vpc_id = vpc_id
+        self._region = region
+        self._aws_access_key = aws_access_key
+        self._aws_access_secret_key = aws_access_secret_key
+        self._vpc_cidr_block = vpc_cidr_block
