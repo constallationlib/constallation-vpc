537a22906bf2cd92acd4c38f346fbf738ef05ce0
CoulterStutz <coultercash@gmail.com>
2024-08-09 17:11:25 -0600

    Auto-commit at Fri Aug  9 05:11:25 PM MDT 2024

--- .github/testscripts/vpc_test.py
+++ .github/testscripts/vpc_test.py
@@ -1,5 +1,5 @@
 import constellation_vpc as vpc
 
-testsubnet = vpc.VPC(region="us-west-2", vpc_id="vpc-08ed1a1cd357c0dab", aws_access_key="ASIASL5DHQSM5TADKAPU", aws_access_secret_key="PV4Coyrj6S/6rOJch9S4O4wGM5IVqr9/daUGxAaR")
-for x in testsubnet.subnets:
+testvpc = vpc.VPC(region="us-west-2", vpc_id="vpc-08ed1a1cd357c0dab", aws_access_key="ASIASL5DHQSM5TADKAPU", aws_access_secret_key="PV4Coyrj6S/6rOJch9S4O4wGM5IVqr9/daUGxAaR")
+for x in testvpc.subnets:
     print(x)
