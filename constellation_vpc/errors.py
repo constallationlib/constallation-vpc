57601cad2531800f52d00ad896236045b6f03353
CoulterStutz <coultercash@gmail.com>
2024-08-08 21:42:26 -0600

    Auto-commit at Thu Aug  8 09:42:26 PM MDT 2024

--- constellation_vpc/errors.py
+++ constellation_vpc/errors.py
@@ -1,10 +1,12 @@
 import re as _re
 
+
 class SubnetError(Exception):
     def __init__(self, operation, message, *args):
         self.operation = operation
         self.message = message
         super().__init__(message, *args)
+
 
 class SubnetCIDRConflicts(SubnetError):
     def __init__(self, operation, original_message, *args):
@@ -17,6 +19,7 @@ class SubnetCIDRConflicts(SubnetError):
         super().__init__(operation, formatted_message, *args)
         self.error_code = "InvalidSubnet.Conflict"
 
+
 class SubnetRangeError(SubnetError):
     def __init__(self, operation, original_message, *args):
         if (range_match := _re.search(r"'(.*?)'", original_message)):
@@ -28,11 +31,13 @@ class SubnetRangeError(SubnetError):
         super().__init__(operation, formatted_message, *args)
         self.error_code = "InvalidSubnet.Range"
 
+
 class SubnetIDNotFound(SubnetError):
     def __init__(self, operation, original_message, *args):
         formatted_message = "The specified subnet ID was not found."
         super().__init__(operation, formatted_message, *args)
         self.error_code = "InvalidSubnet.ID.NotFound"
+
 
 class SubnetZoneMismatch(SubnetError):
     def __init__(self, operation, original_message, *args):
@@ -40,11 +45,13 @@ class SubnetZoneMismatch(SubnetError):
         super().__init__(operation, formatted_message, *args)
         self.error_code = "InvalidSubnet.ZoneMismatch"
 
+
 class SubnetInUse(SubnetError):
     def __init__(self, operation, original_message, *args):
         formatted_message = "The subnet is currently in use and cannot be modified or deleted."
         super().__init__(operation, formatted_message, *args)
         self.error_code = "InvalidSubnet.InUse"
+
 
 class SubnetAssociationError(SubnetError):
     def __init__(self, operation, original_message, *args):
@@ -52,17 +59,98 @@ class SubnetAssociationError(SubnetError):
         super().__init__(operation, formatted_message, *args)
         self.error_code = "InvalidSubnet.Association"
 
+
 class SubnetDependentServiceError(SubnetError):
     def __init__(self, operation, original_message, *args):
         formatted_message = "The subnet is being used by a dependent service and cannot be modified or deleted."
         super().__init__(operation, formatted_message, *args)
         self.error_code = "InvalidSubnet.DependentService"
 
+
 class SubnetAttachmentError(SubnetError):
     def __init__(self, operation, original_message, *args):
         formatted_message = "There was an error attaching the subnet to the specified resource."
         super().__init__(operation, formatted_message, *args)
         self.error_code = "InvalidSubnet.Attachment"
+
+
+# General AWS Errors
+class AccessDeniedError(Exception):
+    def __init__(self, operation, message, *args):
+        formatted_message = "Access denied. You do not have the necessary permissions for this operation."
+        super().__init__(operation, formatted_message, *args)
+        self.error_code = "AccessDenied"
+
+
+class AuthFailureError(Exception):
+    def __init__(self, operation, message, *args):
+        formatted_message = "Authentication failed. Please check your AWS credentials."
+        super().__init__(operation, formatted_message, *args)
+        self.error_code = "AuthFailure"
+
+
+class RequestLimitExceededError(Exception):
+    def __init__(self, operation, message, *args):
+        formatted_message = "Request limit exceeded. Please wait and try again later."
+        super().__init__(operation, formatted_message, *args)
+        self.error_code = "RequestLimitExceeded"
+
+
+class ThrottlingExceptionError(Exception):
+    def __init__(self, operation, message, *args):
+        formatted_message = "Request was throttled. Please wait and try again later."
+        super().__init__(operation, formatted_message, *args)
+        self.error_code = "ThrottlingException"
+
+
+class ResourceNotFoundExceptionError(Exception):
+    def __init__(self, operation, message, *args):
+        formatted_message = "The specified resource could not be found."
+        super().__init__(operation, formatted_message, *args)
+        self.error_code = "ResourceNotFoundException"
+
+
+class InvalidParameterValueError(Exception):
+    def __init__(self, operation, message, *args):
+        formatted_message = "One or more parameters are invalid. Please check the input values."
+        super().__init__(operation, formatted_message, *args)
+        self.error_code = "InvalidParameterValue"
+
+
+class ServiceUnavailableError(Exception):
+    def __init__(self, operation, message, *args):
+        formatted_message = "The service is currently unavailable. Please try again later."
+        super().__init__(operation, formatted_message, *args)
+        self.error_code = "ServiceUnavailable"
+
+
+class InternalFailureError(Exception):
+    def __init__(self, operation, message, *args):
+        formatted_message = "An internal error occurred. Please try again later."
+        super().__init__(operation, formatted_message, *args)
+        self.error_code = "InternalFailure"
+
+
+class ValidationExceptionError(Exception):
+    def __init__(self, operation, message, *args):
+        formatted_message = "The input provided does not meet the required constraints."
+        super().__init__(operation, formatted_message, *args)
+        self.error_code = "ValidationException"
+
+
+class InvalidClientTokenIdError(Exception):
+    def __init__(self, operation, message, *args):
+        formatted_message = "The security token included in the request is invalid."
+        super().__init__(operation, formatted_message, *args)
+        self.error_code = "InvalidClientTokenId"
+
+
+class OptInRequiredError(Exception):
+    def __init__(self, operation, message, *args):
+        formatted_message = "You must opt in to use this service."
+        super().__init__(operation, formatted_message, *args)
+        self.error_code = "OptInRequired"
+
 
 class ErrorHandler:
     def parse_and_raise(self, error):
@@ -74,27 +162,4 @@ class ErrorHandler:
         operation_match = _re.search(r'when calling the (.*?) operation', error_message)
 
         if code_match and operation_match:
-            error_code = code_match.group(1)
-            operation = operation_match.group(1)
 
-            # Raise the appropriate error based on the error code
-            if error_code == "InvalidSubnet.Conflict":
-                raise SubnetCIDRConflicts(operation, error_message)
-            elif error_code == "InvalidSubnet.Range":
-                raise SubnetRangeError(operation, error_message)
-            elif error_code == "InvalidSubnet.ID.NotFound":
-                raise SubnetIDNotFound(operation, error_message)
-            elif error_code == "InvalidSubnet.ZoneMismatch":
-                raise SubnetZoneMismatch(operation, error_message)
-            elif error_code == "InvalidSubnet.InUse":
-                raise SubnetInUse(operation, error_message)
-            elif error_code == "InvalidSubnet.Association":
-                raise SubnetAssociationError(operation, error_message)
-            elif error_code == "InvalidSubnet.DependentService":
-                raise SubnetDependentServiceError(operation, error_message)
-            elif error_code == "InvalidSubnet.Attachment":
-                raise SubnetAttachmentError(operation, error_message)
-            else:
-                raise SubnetError(operation, error_message)
-        else:
-            raise SubnetError("UnknownOperation", error_message)
