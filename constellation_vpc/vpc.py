4fee16f0367cc9b681deacb176655648ceb2abda
CoulterStutz <coultercash@gmail.com>
2024-08-09 13:56:58 -0600

    Auto-commit at Fri Aug  9 01:56:58 PM MDT 2024

--- constellation_vpc/vpc.py
+++ constellation_vpc/vpc.py
@@ -10,10 +10,11 @@ class VPC(_vpc):
         self._vpc_cidr_block = vpc_cidr_block
         super().__init__(region=region, aws_access_key=self._aws_access_key, aws_access_secret_key=self._aws_access_secret_key, aws_sts_session_token=self._aws_sts_token)
         del self._aws_sts_token, self._aws_access_key, self._aws_access_secret_key
+        self._initialize_vpc()
 
     def _initialize_vpc(self):
         if self._vpc_id is not None:
             print(super()._describe_vpc(self._vpc_id))
 
 if __name__ == '__main__':
-vpc = VPC('us-east-1')
+    vpc = VPC('us-east-1', vpc_id="vpc-017f9600d16474436")
